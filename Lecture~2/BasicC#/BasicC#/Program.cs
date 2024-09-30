523476771fd98bd384e28b71a89e01a00df29771
chinmoy <chinmoy.bhowmik.cuet@gmail.com>
2024-09-30 12:56:20 +0600

    Provide Class Lectures

---
+++ Lecture~2/BasicC#/BasicC#/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//type casting 
+
+//implicit typecasting
+//int b = 15;
+//double a;
+//a = b;
+
+//explicit typecasting
+//int bc;
+//double ac = 15.2D;
+//bc = (int)ac;
+
+//int xyz = Convert.ToInt32(ac);
+//scan an input
+
+//int name = Convert.ToInt32(Console.ReadLine());
+
+//foreach loop
+//int[] arr = { 1, 2, 3, 4, 5};
+
+//foreach (int item in arr)
+//{
+//    Console.WriteLine(item);
+//}
+
+//binary search implementation
+
+int[] array = { 1, 2, 3, 4, 8, 0 };
+Array.Sort(array);
+int key = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(BinarySearch(array, key));
+static string BinarySearch(int[] arr, int key)
+{
+    int min = 0;
+    int max = arr.Length - 1;
+    int mid = 0;
+
+    while (min <= max)
+    {
+        mid = (min + max) / 2;
+
+        if (key == arr[mid])
+            return "Found";
+        else if (key < arr[mid])
+            max = mid - 1;
+        else if (key > arr[mid])
+            min = mid + 1;
+
+    }
+    return "Not Found";
+
+
+
+}
---
+++ Lecture~2/BasicC#/ReOOP/Program.cs
@@ -0,0 +1,221 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//class + Object Initialization
+
+//Person person = new Person();
+//Console.WriteLine(person.age);
+//class Person
+//{
+//    public string name;   //fields
+//    public int age = 2;
+//    //properties
+//    public void PrintSomething() //metod
+//    {
+//        Console.WriteLine("Hi");
+//    }
+//}
+
+//constructor
+//Person person = new Person();
+//Console.WriteLine(person.name);
+
+//Person person2 = new Person("rahul", 10);
+//Console.WriteLine(person2.name);
+
+//class Person
+//{
+//    public string name;   //fields
+//    public int age = 2;
+//    //properties
+
+//    public Person() //default constructor
+//    {
+//        name = "chinmoy";
+//        age = 28;
+//    }
+//    public Person(string Name, int Age) //parametrized constructor
+//    {
+//        name = Name;
+//        age = Age;
+//    }
+//    public void PrintSomething() //metod
+//    {
+//        Console.WriteLine("Hi");
+//    }
+//}
+
+
+//Access Modifier
+
+//public
+//Person person = new Person();
+//Console.WriteLine(person.age);
+//Person person2 = new Person();
+//Console.WriteLine(person2.GetPrivateVal());
+
+//Person person3 = new Person();
+//Console.WriteLine(person3.Pwd);
+
+//class Person
+//{
+//    public string name;   //fields
+//    public int age = 2;
+//    private string pwd = "1234";
+
+//    public string Pwd { get { return pwd; } set { pwd = value;  } }
+
+//    public string GetPrivateVal()
+//    { return pwd; }
+
+//}
+
+//static
+
+//Person  person = new Person();
+//Console.WriteLine(person.GetPrivateVal());
+
+//Person person2 = new Person();
+//Console.WriteLine(person2.GetPrivateVal());
+
+//Person person3 = new Person();
+//Console.WriteLine(person3.GetPrivateVal());
+//class Person
+//{
+
+//    public static int a = 2;
+//    public int GetPrivateVal()
+//    { 
+//        return ++a; 
+//    }
+
+//}
+
+//inhitance
+//Person2 person = new Person2();
+//Console.WriteLine(person.a);
+//class Person
+//{
+
+//    public int a = 2;
+
+
+//}
+//class Person2 : Person
+//{
+
+//    public int GetPrivateVal()
+//    {
+//        return ++a;
+//    }
+
+//}
+
+//method overloading
+
+//Person person = new Person();
+//person.Sum(1, 2);
+//person.Sum(1.1D, 2.2D);
+//person.Sum(1.1F, 2.1F);
+
+//class Person
+//{
+
+//    public void Sum(int a, int b)
+//    {
+//        Console.WriteLine("I am from int");
+//    }
+//    public void Sum(double a, double b)
+//    {
+//        Console.WriteLine("I am from double");
+//    }
+//    public void Sum(float a, float b)
+//    {
+//        Console.WriteLine("I am from float");
+//    }
+
+
+//}
+
+
+
+//method overriding
+
+//class Person
+//{
+
+//    virtual public void Sum(int a, int b)
+//    {
+//        Console.WriteLine("I am from main");
+//    }
+
+
+
+//}
+
+//class Person2 : Person
+//{
+//    override public void Sum(int a, int b)
+//    {
+//        Console.WriteLine("I am from Overrided One");
+//    }
+//}
+
+//abstraction
+
+
+//abstract class Person
+//{
+//    public int ReturnSomething()
+//    {
+//        return 1;
+//    }
+//    abstract public void Print();
+
+//}
+//class P2 : Person
+//{
+//    override public void Print()
+//    {
+//        Console.WriteLine("");
+//    }
+//}
+
+//Interface
+
+interface IPerson
+{
+    public void Nothing();
+}
+interface IPerson2
+{
+    public void Nothing();
+}
+
+
+class P1 : IPerson
+{
+    public void Nothing()
+    {
+        
+    }
+}
+class P2
+{
+    public void Nothing()
+    {
+
+    }
+}
+class p3 : IPerson, IPerson2
+{
+    public void Nothing()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+//multiple inheritance
+//multilevel inheritance
+
