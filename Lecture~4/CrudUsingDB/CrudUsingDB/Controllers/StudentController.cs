f721c4ded871e7960b314f88ec2f049ad5f135e4
chinmoy <chinmoy.bhowmik.cuet@gmail.com>
2024-11-22 20:43:46 +0600

    22/11/2024~ch1nm0y

---
+++ Lecture~4/CrudUsingDB/CrudUsingDB/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using CrudUsingDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CrudUsingDB.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var conString = "Server=localhost;Database=20B1;Trusted_Connection=True;MultipleActiveResultSets=true";
+            List<Student> std = new List<Student>();
+
+
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                string query = "select * from Student";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    std.Add(new Student
+                    {
+                        Id = (int)reader["Id"],
+                        Name = (string)reader["Name"],
+                        Address = (string)reader["Address"],
+                        Age = (int)reader["Age"],
+                    });
+                }
+            }
+            return View(std);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student std)
+        {
+            var conString = "Server=localhost;Database=20B1;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                string query = "INSERT INTO Student (Name, Address, Age) VALUES (@parameter1, @parameter2, @parameter3)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@parameter1", std.Name);
+                cmd.Parameters.AddWithValue("@parameter2", std.Address);
+                cmd.Parameters.AddWithValue("@parameter3", std.Age);
+                
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ Lecture~4/WebApplication1/WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employee> emp = new();
+
+            string con = "Server=localhost;Database=20B1;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+            using (SqlConnection conn = new SqlConnection(con))
+            {
+                string query = "select * from Employee";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    emp.Add(new Employee
+                    {
+                        Id = (int)reader["Id"],
+                        Name = reader["Name"].ToString(),
+                        Email = reader["Email"].ToString()
+                    });
+                }
+            }
+
+
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            
+
+            string con = "Server=localhost;Database=20B1;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+            
+
+
+            return Ok();
+        }
+    }
+}
